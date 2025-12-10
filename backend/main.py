18284efb0707bcf65b9f2da88bdaed0216c1205d
Hamza Siddiqui <hamsidhi@gmail.com>
2025-12-10 09:27:16 +0530

    ArchaeoMind Phase 1 MVP COMPLETE! Backend+Frontend+RAG+Groq

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 ﻿import os
 from dotenv import load_dotenv
 load_dotenv()
-os.environ['GROQ_API_KEY'] = "gsk_xxxxxxxx"  
+os.environ['GROQ_API_KEY'] = "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"  
 from fastapi import FastAPI, UploadFile, File, Form
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
